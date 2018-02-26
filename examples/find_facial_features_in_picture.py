6d2ee27605e08b840391a92771bbfe7281907f6b
Matthew Wallace <1502616@abertay.ac.uk>
2018-02-26 13:39:54 +0000

    better string

--- examples/find_facial_features_in_picture.py
+++ examples/find_facial_features_in_picture.py
@@ -2,6 +2,7 @@ from PIL import Image, ImageDraw
 import face_recognition
 import csv
 import numpy as np
+import array
 
 # to run the application in the terminal: 'python3 find_facial_features_in_picture.py'
 
@@ -27,15 +28,16 @@ for face_landmarks in face_landmarks_list:
         'top_lip',
         'bottom_lip'
     ]
-
+    
     for facial_feature in facial_features:
         print("The {} in this face has the following points: {}".format(facial_feature, face_landmarks[facial_feature]))
-
-    for facial_feature in facial_features:
-        string = ("The {} in this face has the following points: {}".format(facial_feature, face_landmarks[facial_feature]))
-        with open('smile.csv', 'w', newline='') as fp:
-            a = csv.writer(fp, delimiter=',')
-            a.writerows(string)
+        myFile = open('smile.csv', 'w')
+        with myFile:
+            writer = csv.writer(myFile)
+            writer.writerows(face_landmarks[facial_feature])
+        #with open('smile.csv', 'w', newline='') as fp:
+        #    a = csv.writer(fp, delimiter='\n')
+        #    a.writerows(face_landmarks[facial_feature])
 
     # Let's trace out each facial feature in the image with a line!
     pil_image = Image.fromarray(image)
@@ -45,6 +47,15 @@ for face_landmarks in face_landmarks_list:
         d.line(face_landmarks[facial_feature], width=1)
 
     pil_image.show()
+
+myData = [["first_name", "second_name", "Grade"],
+          ['Alex', 'Brian', 'A'],
+          ['Tom', 'Smith', 'B']]
+
+myFile = open('example2.csv', 'w')
+with myFile:
+    writer = csv.writer(myFile)
+    writer.writerows(myData)
 
 # mark picture
 #
