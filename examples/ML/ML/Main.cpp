c444a5d2c593b3325a1a3e49dea9976cb224e2b2
Matthew Wallace <1502616@abertay.ac.uk>
2018-02-26 19:19:00 +0000

    data

--- examples/ML/ML/Main.cpp
+++ examples/ML/ML/Main.cpp
@@ -28,6 +28,9 @@ int main()
 	FILE *fp;
 	char str1[10], str2[10];
 	std::vector<int> learning_data;
+	std::vector<int> top_lip_bottom_lip_distance;
+	std::vector<int> left_eyebrow_left_eye_distance;
+	std::vector<int> right_eyebrow_right_eye_distance;
 
 	fp = fopen("../../smile.csv", "r");
 	if (NULL == fp)
@@ -52,6 +55,41 @@ int main()
 			std::cout << "even i: " << i << " learning_data: " << learning_data.at(i) << std::endl;
 		else // place in the learning data vector is odd (1, 3, 5 ... 13)
 			std::cout << "odd  i: " << i << " learning_data: " << learning_data.at(i) << std::endl;
+
+		// chin
+		for (int i = 0; i < 17; ++i)
+
+		// left eyebrow 
+		for (int i = 0; i < 5; ++i)
+
+		// right_eyebrow
+		// nose_bridge 
+		// nose_tip   
+		// left_eye    
+		// right_eye   
+		// top_lip     
+		// bottom_lip
+
+		// data in the facial_recognition algorithm has a layout:
+		/*
+		'chin',          17 * (a, b)
+		'left_eyebrow',  5 * (a, b)
+		'right_eyebrow', 5 * (a, b)
+		'nose_bridge',   4 * (a, b)
+		'nose_tip',      5 * (a, b)
+		'left_eye',      6 * (a, b)
+		'right_eye',     6 * (a, b)
+		'top_lip',       12 * (a, b)
+		'bottom_lip'     12 * (a, b)
+		*/
+		// in order to extract facial features we need to take the appropriate pairs of data and subtrack them so we can get:
+		// a distance of the left_eyebrow from the left_eye 
+
+		// a distance of the right_eyebrow from the right_eye 
+
+		// a distance of the top_lip from the bottom_lip
+
+
 	}
 
 	std::cin.get();
