fabd885daa3a3691496642dd2754a047a43b5929
Matthew Wallace <1502616@abertay.ac.uk>
2018-02-27 17:32:39 +0000

    refactoring

--- examples/ML/ML/Main.cpp
+++ examples/ML/ML/Main.cpp
@@ -22,7 +22,7 @@ const int STEP = 2 * (CHIN + LEFT_EYEBROW + RIGHT_EYEBROW + NOSE_BRIDGE + NOSE_T
 // vector to store the data of the facial features to learn from
 std::vector<int> learning_data;
 
-int GenerateLearningVectorFromAFile()
+int GenerateLearningVectorFromFile()
 {
 	// file to store the .csv 
 	FILE *fp;
@@ -59,7 +59,7 @@ std::vector<int> right_eye;
 std::vector<int> top_lip;
 std::vector<int> bottom_lip;
 
-void PopulateFacialFeaturesVectors()
+void PopulateFacialFeaturesVectors(std::vector<int>& learning_data)
 {
 	// the number of rows to cover all the feautres from one picture is 72 but because there's two colums and they are stored in 1D array we must divide the size 
 	// by 144 to get the number of pictures examined.
@@ -276,27 +276,94 @@ void CalculateEmotionWeightingsForSmile()
 
 }
 
+std::vector<int> facial_features_from_picture; 
+
+void CheckEmotionFromPicture()
+{
+	/*
+	Check emtion from a picture:
+	- Read facial features of a single face from a file
+	- Create facial features vector
+	- Create an emotion weighted vector
+	- Read emotion weighting from a file (in a group of 3)
+	- Create emotion weighted vectors (in a group of 3)
+	- Compare the emotion weighted vector (of a single face) with the emotion weighted vectors (created from the learning file),
+	like smile_weightings_vector, sad_weightenings_vector ect.
+	- The one with the least score is the most likely to be that emotion:
+	smile_probability.push_back(emotion weighted vectors(i) - emotion weight from a single picture)
+	*/
+
+	/* Read facial features of a single face from a file */
+	// file to store the .csv 
+	FILE *fp;
+	// two chars to read from two columns in each row in the .csv file
+	char str1[10], str2[10];
+
+	// open the .csv file to read the data
+	fp = fopen("../../smile_test.csv", "r");
+	// check if the file was opened correctly
+	if (NULL == fp)
+	{
+		printf("\nError in opening file.");
+	}
+	else
+	{
+		// read data into a vector from a .csv file with two columns
+		while (EOF != fscanf(fp, " %[^,], %s, %s ", str1, str2))
+		{
+			/* Create facial features vector*/
+			facial_features_from_picture.push_back(std::stoi(str1));
+			facial_features_from_picture.push_back(std::stoi(str2));
+		}
+	}
+
+	/* Create emotion weighted vector */
+	//facial_features_from_picture
+
+	#if 0
+				/* Read emotion weighting from a file */
+	// close the file after reading the data into the vector
+	fclose(fp);
+
+	// file to store the .csv 
+	FILE *fpe;
+	// two chars to read from two columns in each row in the .csv file
+	char str1[10], str2[10];
+
+	// open the .csv file to read the data
+	fpe = fopen("smile_learning.csv", "r");
+	// check if the file was opened correctly
+	if (NULL == fp)
+	{
+		printf("\nError in opening file.");
+	}
+	else
+	{
+		// read data into a vector from a .csv file with two columns
+		while (EOF != fscanf(fpe, " %[^,], %s, %s ", str1, str2))
+		{
+			/* Create emotion weighting vectors */
+			smile_weightings.push_back(std::stoi(str1));
+			smile_weightings.push_back(std::stoi(str2));
+		}
+	}
+	// close the file after reading the data into the vector
+	fclose(fp);
+	#endif // 0
+
+}
+
 int main()
 {
-	GenerateLearningVectorFromAFile();
+	GenerateLearningVectorFromFile();
 
-	PopulateFacialFeaturesVectors();
+	PopulateFacialFeaturesVectors(learning_data);
 
 	CalculateEmotionWeightingsForSmile();
 
 	DisplayFacialFeaturesVectors();
 
-	/*
-	Check emtion from a picture:
-		- Read facial features of a single face from a file
-		- Create an emotion weighted vector
-		- Read emotion weighted vectors from a file (in a group of 3)
-		- Create emotion weighted vectors (in a group of 3)
-		- Compare the emotion weighted vector (of a single face) with the emotion weighted vectors (created from the learning file), 
-		like smile_weightings_vector, sad_weightenings_vector ect.
-		- The one with the least score is the most likely to be that emotion:
-		smile_probability.push_back(emotion weighted vectors(i) - emotion weight from a single picture)
-	*/
+	CheckEmotionFromPicture();
 
 	m_SmileWeightings.close();
 
