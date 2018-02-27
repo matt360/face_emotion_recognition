6fefbb85c7640ccd6608de2d5f92fb5eb17d4b27
Matthew Wallace <1502616@abertay.ac.uk>
2018-02-27 13:17:58 +0000

    refactored

--- examples/ML/ML/MachineLearning.h
+++ examples/ML/ML/MachineLearning.h
@@ -1,11 +1,5 @@
 #pragma once
 
-#include <fstream>
-#include <sstream>
-#include <iostream>
-#include <vector>
-#include <string>
-
 class MachineLearning
 {
 public:
@@ -20,3 +14,23 @@ public:
 private:
 	//std::ifstream m_FileData;
 };
+
+
+/*
+FILE *fp;
+char str1[10], str2[10], str3[10], str4[10];
+
+fp = fopen("G:\\data1.csv", "r");
+if (NULL == fp)
+{
+printf("\nError in opening file.");
+return 0;
+}
+while (EOF != fscanf(fp, " %[^,], %[^,], %[^,], %s, %s, %s, %s ", str1, str2, str3, str4))
+{
+printf("\n%s %s %s %s", str1, str2, str3, str4);
+}
+fclose(fp);
+
+// [^,], ^ -it inverts logic , means match any string that does not contain comma then last , says to match comma that terminated previous string.
+*/
--- examples/ML/ML/Main.cpp
+++ examples/ML/ML/Main.cpp
@@ -19,25 +19,6 @@ std::ofstream m_Smile;
 
 int main()
 {
-	/*
-	FILE *fp;
-	char str1[10], str2[10], str3[10], str4[10];
-
-	fp = fopen("G:\\data1.csv", "r");
-	if (NULL == fp)
-	{
-	printf("\nError in opening file.");
-	return 0;
-	}
-	while (EOF != fscanf(fp, " %[^,], %[^,], %[^,], %s, %s, %s, %s ", str1, str2, str3, str4))
-	{
-	printf("\n%s %s %s %s", str1, str2, str3, str4);
-	}
-	fclose(fp);
-
-	// [^,], ^ -it inverts logic , means match any string that does not contain comma then last , says to match comma that terminated previous string.
-	*/
-
 	// std::ios_bas::app - All output operations happen at the end of the file, appending to its existing contents.
 	m_Smile.open("smile_learning.csv", std::ios_base::app);
 
@@ -57,28 +38,30 @@ int main()
 	std::vector<int> right_eyebrow_right_eye_distance;
 	*/
 
+	// file to store the .csv 
 	FILE *fp;
+	// two chars to read from two columns in each row in the .csv file
 	char str1[10], str2[10];
+	// vector to store the data of the facial features to learn from
 	std::vector<int> learning_data;
 
+	// open the .csv file to read the data
 	fp = fopen("../../smile.csv", "r");
+	// check if the file was opened correctly
 	if (NULL == fp)
 	{
 		printf("\nError in opening file.");
 		return 0;
 	}
 
+	// read data into a vector from a .csv file with two columns
 	while (EOF != fscanf(fp, " %[^,], %s, %s ", str1, str2))
 	{
 		learning_data.push_back(std::stoi(str1));
 		learning_data.push_back(std::stoi(str2));
 	}
+	// close the file after reading the data into the vector
 	fclose(fp);
-
-	/*
-	for (auto c : learning_data)
-		std::cout << c << std::endl;
-	*/
 
 	// the number of rows to cover all the feautres from one picture is 72 but because there's two colums and they are stored in 1D array we must divide the size 
 	// by 144 to get the number of pictures examined.
@@ -106,84 +89,64 @@ int main()
 		//std::cout << "nose bridge" << std::endl;
 		//for (int i = (17 * 2 + 5 * 2 + 5 * 2); i < (17 * 2 + 5 * 2 + 5 * 2 + 4 * 2); i += 2)
 		for (int i = 54; i < 62; i++)
-		{
-			/*std::cout << learning_data.at(i) << ", ";
-			std::cout << learning_data.at(i + 1) << std::endl;*/
 			nose_bridge.push_back(learning_data.at(i));
-		}
+
 		// nose_tip (5) [32]
 		//std::cout << "nose tip" << std::endl;
 		//for (int i = (17 * 2 + 5 * 2 + 5 * 2 + 4 * 2); i < (17 * 2 + 5 * 2 + 5 * 2 + 4 * 2 + 5 * 2); i += 2)
 		for (int i = 62; i < 72; i++)
-		{
-			/*std::cout << learning_data.at(i) << ", ";
-			std::cout << learning_data.at(i + 1) << std::endl;*/
 			nose_tip.push_back(learning_data.at(i));
-		}
+		
 		// left_eye (6) [37]
 		//std::cout << "left eye" << std::endl;
 		//for (int i = (17 * 2 + 5 * 2 + 5 * 2 + 4 * 2 + 5 * 2); i < (17 * 2 + 5 * 2 + 5 * 2 + 4 * 2 + 5 * 2 + 6 * 2); i += 2)
 		for (int i = 72; i < 84; i++)
-		{
-			/*std::cout << learning_data.at(i) << ", ";
-			std::cout << learning_data.at(i + 1) << std::endl;*/
 			left_eye.push_back(learning_data.at(i));
-		}
+		
 		// right_eye (6) [43]
 		//std::cout << "right eye" << std::endl;
 		//for (int i = (17 * 2 + 5 * 2 + 5 * 2 + 4 * 2 + 5 * 2 + 6 * 2); i < (17 * 2 + 5 * 2 + 5 * 2 + 4 * 2 + 5 * 2 + 6 * 2 + 6 * 2); i += 2)
 		for (int i = 84; i < 96; i++)
-		{
-			/*std::cout << learning_data.at(i) << ", ";
-			std::cout << learning_data.at(i + 1) << std::endl;*/
 			right_eye.push_back(learning_data.at(i));
-		}
+		
 		// top_lip (12) [49]
 		//std::cout << "top lip" << std::endl;
 		//for (int i = (17 * 2 + 5 * 2 + 5 * 2 + 4 * 2 + 5 * 2 + 6 * 2 + 6 * 2); 
 		//     i < (17 * 2 + 5 * 2 + 5 * 2 + 4 * 2 + 5 * 2 + 6 * 2 + 6 * 2 + 12 * 2); i += 2)
 		for (int i = 96; i < 120; i++)
-		{
-			/*std::cout << learning_data.at(i) << ", ";
-			std::cout << learning_data.at(i + 1) << std::endl;*/
 			top_lip.push_back(learning_data.at(i));
-		}
+		
 		// bottom_lip (12) [61]
 		//std::cout << "bottom lip" << std::endl;
 		//for (int i = (17 * 2 + 5 * 2 + 5 * 2 + 4 * 2 + 5 * 2 + 6 * 2 + 6 * 2 + 12 * 2); 
 		//     i < (17 * 2 + 5 * 2 + 5 * 2 + 4 * 2 + 5 * 2 + 6 * 2 + 6 * 2 + 12 * 2 + 12 *  2); i += 2)
 		for (int i = 120; i < 144; i++)
-		{
-			/*std::cout << learning_data.at(i) << ", ";
-			std::cout << learning_data.at(i + 1) << std::endl;*/
 			bottom_lip.push_back(learning_data.at(i));
-		}
-
-		// data in the facial_recognition algorithm has a layout:
-		/*
-		'chin',          17 * (a, b)
-		'left_eyebrow',  5 * (a, b)
-		'right_eyebrow', 5 * (a, b)
-		'nose_bridge',   4 * (a, b)
-		'nose_tip',      5 * (a, b)
-		'left_eye',      6 * (a, b)
-		'right_eye',     6 * (a, b)
-		'top_lip',       12 * (a, b)
-		'bottom_lip'     12 * (a, b)
-		*/
-		// in order to extract facial features we need to take the appropriate pairs of data and subtrack them so we can get:
-		// a distance of the left_eyebrow from the left_eye 
-		// 5 - 6  = (5 pairs of numbers) - (6 pairs of numbers) = (even - odd) + ... + (even - odd) = 
-		// (0 - 1) + (2 - 3) + (4 - 5) + (6 - 7) + (8 - 9) = -(weight value for the emotion)
-		//                                                      
-		// a distance of the right_eyebrow from the right_eye 
-		// 5 - 6 (
-		// a distance of the top_lip from the bottom_lip
-		// 12 - 12
-		// nose tip - nose bridge
-		// 5 - 4
 	}
 
+	// data in the facial_recognition algorithm has a layout:
+	/*
+	'chin',          17 * (a, b)
+	'left_eyebrow',  5 * (a, b)
+	'right_eyebrow', 5 * (a, b)
+	'nose_bridge',   4 * (a, b)
+	'nose_tip',      5 * (a, b)
+	'left_eye',      6 * (a, b)
+	'right_eye',     6 * (a, b)
+	'top_lip',       12 * (a, b)
+	'bottom_lip'     12 * (a, b)
+	*/
+	// in order to extract facial features we need to take the appropriate pairs of data and subtrack them so we can get:
+	// a distance of the left_eyebrow from the left_eye 
+	// 5 - 6  = (5 pairs of numbers) - (6 pairs of numbers) = (even - odd) + ... + (even - odd) = 
+	// (0 - 1) + (2 - 3) + (4 - 5) + (6 - 7) + (8 - 9) = -(weight value for the emotion)
+	//                                                      
+	// a distance of the right_eyebrow from the right_eye 
+	// 5 - 6 (
+	// a distance of the top_lip from the bottom_lip
+	// 12 - 12
+	// nose tip - nose bridge
+	// 5 - 4
 	// append emotion weighted values (emv) for the smile
 	// if (emv != any set of values in the file)
 	m_Smile << 1 << ',' << 2 << ',' << 3 << std::endl; // ',' - makes it go to the next cell in the .csv file
