6ed51bd065c40eba62ba06bfa2a5e41cf9fd4b5f
Matthew Wallace <1502616@abertay.ac.uk>
2018-03-05 14:27:24 +0000

    removed unneccessary code

--- examples/ML/ML/FacialFeatures.h
+++ examples/ML/ML/FacialFeatures.h
@@ -20,11 +20,7 @@ public:
     void CalculateEmotionWeightings(const char* file_name);
     void CalculateEmotionWeightings();
 
-    // emotion weighted values (emv)
-    //std::vector<int> top_lip_bottom_lip_distance;
-    //std::vector<int> left_eyebrow_left_eye_distance;
-    //std::vector<int> right_eyebrow_right_eye_distance;
-    //std::vector<int> nose_tip_nose_bridge_distance;
+    // emotion weighted values (ewv)
     std::vector<int> weightingsVector;
     // to store smile weightings
     std::ofstream weightingsFile;
--- examples/ML/ML/MachineLearning.h
+++ examples/ML/ML/MachineLearning.h
@@ -5,7 +5,7 @@
 #include "PictureFeatures.h"
 #include <map>
 
-#define GET_VAR_NAME(var) #var
+//#define GET_VAR_NAME(var) #var
 
 class MachineLearning
 {
@@ -23,24 +23,3 @@ private:
    SmileFeatures smileFeatures;
    PictureFeatures pictureFeatures;
 };
-	
-
-
-/*
-FILE *fp;
-char str1[10], str2[10], str3[10], str4[10];
-
-fp = fopen("G:\\data1.csv", "r");
-if (NULL == fp)
-{
-printf("\nError in opening file.");
-return 0;
-}
-while (EOF != fscanf(fp, " %[^,], %[^,], %[^,], %s, %s, %s, %s ", str1, str2, str3, str4))
-{
-printf("\n%s %s %s %s", str1, str2, str3, str4);
-}
-fclose(fp);
-
-// [^,], ^ -it inverts logic , means match any string that does not contain comma then last , says to match comma that terminated previous string.
-*/
