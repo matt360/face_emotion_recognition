00bfbd113d47bf02eb2bab2294b4c56fcbf72b39
Mateusz Zaremba <matzaremba@icloud.com>
2018-04-13 22:47:36 +0200

    source

--- examples/face_recognition_test.py
+++
@@ -1,5 +0,0 @@
-import face_recognition
-
-image = face_recognition.load_image_file("6.jpg")
-face_locations = face_recognition.face_locations(image)
-print("I found {} face(s) in this photograph.".format(len(face_locations)))
